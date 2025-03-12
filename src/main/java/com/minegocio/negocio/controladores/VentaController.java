4ccf222ee37c74e5fdf1f5c5bda40630cbdab94f
Tebanqito <yemesesteban@gmail.com>
2025-03-12 16:55:29 -0300

    filtrado de fechas en VentaController

--- src/main/java/com/minegocio/negocio/controladores/VentaController.java
+++ src/main/java/com/minegocio/negocio/controladores/VentaController.java
@@ -7,13 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -37,6 +33,30 @@ public class VentaController {
                 .body(pdf);
     }
 
+    @GetMapping("/por-fecha")
+    public List<Venta> obtenerPorFecha(@RequestParam String fecha) {
+        return ventaService.obtenerPorFecha(LocalDate.parse(fecha));
+    }
+
+    @GetMapping("/por-rango-fechas")
+    public List<Venta> obtenerPorRangoFechas(@RequestParam String desde, @RequestParam String hasta) {
+        return ventaService.obtenerPorRangoFechas(LocalDate.parse(desde), LocalDate.parse(hasta));
+    }
+
+    @GetMapping("/por-cliente")
+    public List<Venta> obtenerPorCliente(@RequestParam String nombre) {
+        return ventaService.obtenerPorCliente(nombre);
+    }
+
+    @GetMapping("/por-monto")
+    public List<Venta> obtenerPorMontoMinimo(@RequestParam double minimo) {
+        return ventaService.obtenerPorMontoMinimo(minimo);
+    }
+
+    @GetMapping("/por-producto")
+    public List<Venta> obtenerPorProducto(@RequestParam String nombre) {
+        return ventaService.obtenerPorProducto(nombre);
+    }
 
     @PostMapping
     public Venta registrarVenta(@RequestBody VentaRequest request) {
