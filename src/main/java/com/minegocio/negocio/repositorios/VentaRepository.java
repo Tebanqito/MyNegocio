d4a862b3c8c98186a609ed4f357735d5763a69d4
Tebanqito <yemesesteban@gmail.com>
2025-03-12 16:45:07 -0300

    filtrado de ventas en VentaRepository

--- src/main/java/com/minegocio/negocio/repositorios/VentaRepository.java
+++ src/main/java/com/minegocio/negocio/repositorios/VentaRepository.java
@@ -2,5 +2,20 @@ package com.minegocio.negocio.repositorios;
 
 import com.minegocio.negocio.entidades.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface VentaRepository extends JpaRepository<Venta, Long> {}
+import java.time.LocalDate;
+import java.util.List;
+
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+    List<Venta> findByFechaBetween(LocalDate inicio, LocalDate fin);
+
+    List<Venta> findByClienteIgnoreCase(String cliente);
+
+    // Filtrar por monto mínimo
+    List<Venta> findByTotalGreaterThanEqual(double total);
+
+    @Query("SELECT v FROM Venta v JOIN v.detalles d WHERE d.producto.nombre = :nombreProducto")
+    List<Venta> findByProducto(@Param("nombreProducto") String nombreProducto);
+}
