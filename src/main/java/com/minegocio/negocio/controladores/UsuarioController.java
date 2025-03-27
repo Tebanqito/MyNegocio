229be0897882d7e362ec9769f2e2edc9df72f7e0
Tebanqito <yemesesteban@gmail.com>
2025-03-27 16:21:08 -0300

    UsuarioController

---
+++ src/main/java/com/minegocio/negocio/controladores/UsuarioController.java
@@ -0,0 +1,29 @@
+package com.minegocio.negocio.controladores;
+
+import com.minegocio.negocio.servicios.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/registrar")
+    public Map<String, String> registrar(@RequestParam String username, @RequestParam String password) {
+        Map<String, String> response = new HashMap<>();
+        if (usuarioService.hayUsuarioRegistrado()) {
+            response.put("mensaje", "Ya existe un usuario registrado. No se pueden crear más cuentas.");
+        } else if (usuarioService.registrarUsuario(username, password)) {
+            response.put("mensaje", "Usuario registrado con éxito.");
+        } else {
+            response.put("mensaje", "Error al registrar usuario.");
+        }
+        return response;
+    }
+}
