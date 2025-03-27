77e150ee933bd665d44826264c3a908ad7dd29a4
Tebanqito <yemesesteban@gmail.com>
2025-03-27 16:19:06 -0300

    creacion UsuarioService

---
+++ src/main/java/com/minegocio/negocio/servicios/UsuarioService.java
@@ -0,0 +1,35 @@
+package com.minegocio.negocio.servicios;
+
+import com.minegocio.negocio.entidades.Usuario;
+import com.minegocio.negocio.repositorios.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean registrarUsuario(String username, String password) {
+        if (usuarioRepository.existsByUsername(username)) {
+            return false;
+        }
+        Usuario usuario = new Usuario(username, passwordEncoder.encode(password));
+        usuarioRepository.save(usuario);
+        return true;
+    }
+
+    public Optional<Usuario> buscarPorUsername(String username) {
+        return usuarioRepository.findByUsername(username);
+    }
+
+    public boolean hayUsuarioRegistrado() {
+        return usuarioRepository.count() > 0;
+    }
+}
